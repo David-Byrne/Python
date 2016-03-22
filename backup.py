841cb53c1a2f8de244f212923c859508255010b4
David Byrne <David.dbyrne@gmail.com>
2016-03-22 19:35:22 +0000

    Adds visual studio directory to be backed up

--- backup.py
+++ backup.py
@@ -1,16 +1,39 @@
 import shutil, os
 
-source = 'C:\\Users\\david\workspace'
+backup_dest = "C:\\Users\\david\\Desktop\\Backup\\"
 
-for folderName, subfolders, filenames in os.walk(source):
-    # print('The current folder is ' + folderName)
+def visual_studio():
+    source = 'C:\\Users\\david\\Documents\\Visual Studio 2013'
 
-    for filename in filenames:
-        if filename.endswith(".java"):
-            dest = "C:\\Users\\david\\Desktop\\temp"+folderName[len(source):]
-            # copies in the rest of the file structure after what's given in the source
+    for folderName, subfolders, filenames in os.walk(source):
+        # print('The current folder is ' + folderName)
 
-            if not os.path.exists(dest):
-                os.makedirs(dest)  # if the folder structure doesn't already exist create it now
+        for filename in filenames:
+            if filename.endswith(".c") or filename.endswith(".h"):
+                dest = backup_dest+"VS2013"+folderName[len(source):]
+                # copies in the rest of the file structure after what's given in the source
 
-            shutil.copy(folderName + "\\" + filename, dest)  # copies the file into the location
+                if not os.path.exists(dest):
+                    os.makedirs(dest)  # if the folder structure doesn't already exist create it now
+
+                shutil.copy(folderName + "\\" + filename, dest)  # copies the file into the location
+
+
+def eclipse():
+    source = 'C:\\Users\\david\workspace'
+
+    for folderName, subfolders, filenames in os.walk(source):
+        # print('The current folder is ' + folderName)
+
+        for filename in filenames:
+            if filename.endswith(".java"):
+                dest = backup_dest+"Eclipse"+folderName[len(source):]
+                # copies in the rest of the file structure after what's given in the source
+
+                if not os.path.exists(dest):
+                    os.makedirs(dest)  # if the folder structure doesn't already exist create it now
+
+                shutil.copy(folderName + "\\" + filename, dest)  # copies the file into the location
+
+eclipse()
+visual_studio()
