dc729d65b23b3fdbbd2e30d58b0fa205cef8720d
David Byrne <David.dbyrne@gmail.com>
2016-03-23 20:55:57 +0000

    Makes a generic backup function
    Replaces the custom function for each directory with a generic backup
    function that takes in parameters that determines what it backs up to
    where.

--- backup.py
+++ backup.py
@@ -1,16 +1,14 @@
 import shutil, os
 
-backup_dest = "C:\\Users\\david\\Desktop\\Backup\\"
 
-def visual_studio():
-    source = 'C:\\Users\\david\\Documents\\Visual Studio 2013'
+def backup(source, destination, valid_extensions, excluded_files):
 
     for folderName, subfolders, filenames in os.walk(source):
         # print('The current folder is ' + folderName)
 
         for filename in filenames:
-            if filename.endswith(".c") or filename.endswith(".h"):
-                dest = backup_dest+"VS2013"+folderName[len(source):]
+            if filename.endswith(valid_extensions) and filename not in excluded_files:
+                dest = destination+folderName[len(source):]
                 # copies in the rest of the file structure after what's given in the source
 
                 if not os.path.exists(dest):
@@ -19,21 +17,8 @@ def visual_studio():
                 shutil.copy(folderName + "\\" + filename, dest)  # copies the file into the location
 
 
-def eclipse():
-    source = 'C:\\Users\\david\workspace'
-
-    for folderName, subfolders, filenames in os.walk(source):
-        # print('The current folder is ' + folderName)
-
-        for filename in filenames:
-            if filename.endswith(".java"):
-                dest = backup_dest+"Eclipse"+folderName[len(source):]
-                # copies in the rest of the file structure after what's given in the source
-
-                if not os.path.exists(dest):
-                    os.makedirs(dest)  # if the folder structure doesn't already exist create it now
-
-                shutil.copy(folderName + "\\" + filename, dest)  # copies the file into the location
-
-eclipse()
-visual_studio()
+backup("C:\\Users\\david\workspace", "C:\\Users\\david\\Desktop\\Backup\\Eclipse", (".java",), ())
+backup("C:\\Users\\david\\Documents\\Visual Studio 2013", "C:\\Users\\david\\Desktop\\Backup\\VS2013",
+       (".c", ".cpp", ".h", ".txt", ".dat", ".csv"), ("stdafx.h", "targetver.h", "stdafx.cpp", "ReadMe.txt"))
+backup("C:\\Users\\david\\Documents\\Visual Studio 2015", "C:\\Users\\david\\Desktop\\Backup\\VS2015",
+       (".c", ".cpp", ".h", ".txt", ".dat", ".csv"), ("stdafx.h", "targetver.h", "stdafx.cpp", "ReadMe.txt"))
